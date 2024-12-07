50596b536b5792ecc4dc13ef40e8ae2cf808b8ba
waonpad <waonpad@gmail.com>
2024-12-07 22:58:08 +0900

    一応suspenseで囲む

--- src/app/(frontend)/(root)/page.tsx
+++ src/app/(frontend)/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { ConvertJaSyllabaryColumn } from "@/components/convert-ja-syllabary-column";
 import { ConvertedPostList } from "@/components/converted-post-list";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   alternates: {
@@ -16,7 +17,9 @@ export default async function Page() {
         <header>
           <h1 className="font-bold text-lg">様々な古典インド</h1>
         </header>
-        <ConvertedPostList />
+        <Suspense fallback={<div>読み込み中...</div>}>
+          <ConvertedPostList />
+        </Suspense>
       </section>
     </div>
   );
