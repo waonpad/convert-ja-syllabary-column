d12500f9ab2779e9dcb4faf4aedb597110dc6077
waonpad <waonpad@gmail.com>
2024-12-08 02:40:52 +0900

    コンポーネントきりだし

--- src/app/(frontend)/(root)/page.tsx
+++ src/app/(frontend)/(root)/page.tsx
@@ -1,7 +1,6 @@
 import { ConvertJaSyllabaryColumn } from "@/components/convert-ja-syllabary-column";
-import { ConvertedPostList } from "@/components/converted-post-list";
+import { ConvertedPostListContainer } from "@/components/converted-post-list-container";
 import type { Metadata } from "next";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   alternates: {
@@ -15,14 +14,7 @@ export default async function Page() {
   return (
     <div className="flex flex-col gap-8">
       <ConvertJaSyllabaryColumn />
-      <section className="flex flex-col gap-2">
-        <header>
-          <h1 className="font-bold text-lg">様々な古典インド</h1>
-        </header>
-        <Suspense fallback={<div>読み込み中...</div>}>
-          <ConvertedPostList />
-        </Suspense>
-      </section>
+      <ConvertedPostListContainer />
     </div>
   );
 }
---
+++ src/components/converted-post-list-container.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { ConvertedPostList } from "./converted-post-list";
+
+export const ConvertedPostListContainer = async () => {
+  "use cache";
+
+  return (
+    <section className="flex flex-col gap-2">
+      <header className="flex flex-col justify-between gap-1 sm:flex-row">
+        <h1 className="font-bold text-lg">様々な古典インド</h1>
+        <span className="inline-flex items-end text-sm">クリックすると古典インドっぽくする前を表示できます 🐘</span>
+      </header>
+      <Suspense fallback={<div>読み込み中...</div>}>
+        <ConvertedPostList />
+      </Suspense>
+    </section>
+  );
+};
