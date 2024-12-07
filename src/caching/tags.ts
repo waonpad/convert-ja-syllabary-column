9ac64ae1efb086c8d16e0ef528816c114f46793b
waonpad <waonpad@gmail.com>
2024-12-08 00:40:07 +0900

    キャッシュたぐ追加

---
+++ src/caching/tags.ts
@@ -0,0 +1,14 @@
+/**
+ * キャッシュタグの一覧
+ *
+ * [Directives: use cache | Next.js](
+ * https://nextjs.org/docs/canary/app/api-reference/directives/use-cache)
+ *
+ * [Cannot access data, headers, params, searchParams, or a short-lived cache a Suspense boundary nor a `"use cache"` above it. | Next.js](
+ * https://nextjs.org/docs/messages/next-prerender-missing-suspense)
+ */
+
+/**
+ * 投稿一覧のキャッシュタグ
+ */
+export const CACHE_TAG_POSTS = "posts";
