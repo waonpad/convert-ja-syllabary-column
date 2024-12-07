76a6b5a6a6cddd314b36c457f9674a97fbb4efda
waonpad <waonpad@gmail.com>
2024-12-08 02:54:18 +0900

    なんか追加

--- src/components/converted-post-list-container.tsx
+++ src/components/converted-post-list-container.tsx
@@ -13,6 +13,9 @@ export const ConvertedPostListContainer = async () => {
       <Suspense fallback={<div>読み込み中...</div>}>
         <ConvertedPostList />
       </Suspense>
+      <div className="mt-2 rounded-md border-2 border-black p-4 text-center font-bold">
+        <span>古典インドを投稿してみよう！</span>
+      </div>
     </section>
   );
 };
