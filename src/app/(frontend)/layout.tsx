633c9ef415c5ee50012d429a0bb50eb53f077a87
waonpad <waonpad@gmail.com>
2024-12-08 19:46:26 +0900

    ふったー追加

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,28 @@
+import { ExternalLink } from "@/components/external-link";
+import { clientEnv } from "@/config/env/client";
 import type { ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   "use cache";
 
-  return <main className="mx-auto flex w-[95%] max-w-screen-lg grow flex-col py-2">{children}</main>;
+  return (
+    <>
+      <main className="mx-auto flex w-[95%] max-w-screen-lg grow flex-col py-2">{children}</main>
+      <footer className="mx-auto flex w-[95%] max-w-screen-lg flex-col justify-center gap-1 py-2 sm:flex-row sm:gap-3">
+        <ExternalLink
+          href={`https://github.com/${clientEnv.NEXT_PUBLIC_GITHUB_REPOSITORY}`}
+          className="text-center text-blue-500 text-sm underline hover:text-blue-600 sm:text-base"
+        >
+          GitHubリポジトリ
+        </ExternalLink>
+        <div className="hidden text-sm sm:block sm:text-base">/</div>
+        <ExternalLink
+          href={clientEnv.NEXT_PUBLIC_KUSO_APP_ADVENT_CALENDAR_2024_URL}
+          className="text-center text-blue-500 text-sm underline hover:text-blue-600 sm:text-base"
+        >
+          クソアプリ Advent Calendar 2024の記事
+        </ExternalLink>
+      </footer>
+    </>
+  );
 }
