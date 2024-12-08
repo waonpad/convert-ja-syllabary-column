2181ca5e8765d75699e4ed3c2e6ea04cecf36817
waonpad <waonpad@gmail.com>
2024-12-08 17:12:15 +0900

    prisma postgres使う事にした

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -1,20 +1,8 @@
 import { serverEnv } from "@/config/env/server";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import ws from "ws";
-
-neonConfig.webSocketConstructor = ws;
-
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true;
-
-const connectionString = serverEnv.DATABASE_URL;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (serverEnv.APP_ENV !== "production") globalForPrisma.prisma = prisma;
