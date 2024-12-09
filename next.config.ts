ae8a382e72b492edaa5a17268744b6eb06adcdf7
waonpad <waonpad@gmail.com>
2024-12-09 23:47:59 +0900

    キャッシュのデフォルト設定上書き

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import "./src/config/env/client";
 import "./src/config/env/server";
 import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
+import { INFINITE_CACHE } from "next/dist/lib/constants";
 
 const nextConfig = (
   phase: string,
@@ -19,6 +20,16 @@ const nextConfig = (
     typedRoutes: true,
     dynamicIO: true,
     reactCompiler: true,
+    // https://nextjs.org/docs/canary/app/api-reference/functions/cacheLife#default-cache-profiles
+    cacheLife: {
+      // キャッシュのデフォルト設定を上書き
+      default: {
+        stale: undefined,
+        // 明示的にrevalidateするまでは無限にキャッシュする
+        revalidate: INFINITE_CACHE,
+        expire: INFINITE_CACHE,
+      },
+    },
   },
   pageExtensions: ["ts", "tsx"].flatMap((extension) => {
     const isDevServer = phase === PHASE_DEVELOPMENT_SERVER;
