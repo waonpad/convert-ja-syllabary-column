e1a5c810d42e48781a884f68fc91b3786128f41f
waonpad <waonpad@gmail.com>
2024-12-09 23:23:46 +0900

    不要metadata削除

--- src/app/(frontend)/(root)/page.tsx
+++ src/app/(frontend)/(root)/page.tsx
@@ -1,12 +1,5 @@
 import { ConvertJaSyllabaryColumn } from "@/components/convert-ja-syllabary-column";
 import { ConvertedPostListContainer } from "@/components/converted-post-list-container";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  alternates: {
-    canonical: "/",
-  },
-};
 
 export default async function Page() {
   "use cache";
